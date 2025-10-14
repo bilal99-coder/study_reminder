91ec17161096ab57925b125128f530dc553e95c4
bilal sahli <bilalsahli1999@gmail.com>
2025-10-14 13:19:04 +0200

    add setup and manifest files

---
+++ study_reminders/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+import study_reminders
+
+setup(name='study_reminders',
+      version= '1.0',
+      author='Bilal Sahli',
+      author_email= 'bisah2204@oslomet.no',
+      url= "https://www.bisah.com/study_reminders",
+      packages=['study_reminders'])
