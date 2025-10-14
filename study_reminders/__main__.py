408db31ffa6c03ccb8e7deedc4c602cdb2f7a3e1
bilal sahli <bilalsahli1999@gmail.com>
2025-10-14 11:39:04 +0200

    better printing to give more information

--- study_reminders/__main__.py
+++ study_reminders/__main__.py
@@ -4,10 +4,5 @@ from students_manager import StudentsManager
 import reminder_generator 
 import reminder_sender
 import logger
-import datetime
-
-print(StudentsManager().students)
-print(StudentsManager().get_students())
-
 
 scheduler.schedule_reminders(StudentsManager(), reminder_generator.generate_reminder, reminder_sender.send_reminder, logger.log_reminder)
