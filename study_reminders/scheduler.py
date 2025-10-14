9907a280d951baf7084af4f830408fd34c0853cc
bilal sahli <bilalsahli1999@gmail.com>
2025-10-14 11:44:29 +0200

    fix bug caused by wrong indentation

--- study_reminders/scheduler.py
+++ study_reminders/scheduler.py
@@ -5,11 +5,13 @@ import datetime
 
 def schedule_reminders(students_manager, reminder_generator, reminder_sender, logger):
     """Schedule reminder delivery for each student at their preferred time."""
+    print("##### Started The Student Reminder App #####")
+    
     for student in students_manager.get_students():
         reminder = reminder_generator(student['name'], student['course'])
         schedule.every().day.at(student['preferred_time']).do(lambda s=student, r=reminder: (reminder_sender(s['email'], r), logger(s, r)))
-        print("##### Started The Student Reminder App #####")
-        while True:
-            print(datetime.datetime.now(), "- Checking for new events...")
-            schedule.run_pending()
-            time.sleep(30) # Check every minute        
+
+    while True:
+        print(datetime.datetime.now(), "- Checking for new events...")
+        schedule.run_pending()
+        time.sleep(30) # Check every minute        
